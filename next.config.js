a3b48265a8a5ddebe1b63b98807ffceaefb62ebb
Tony Kwokm <holypaladin07@gmail.com>
2021-08-18 00:57:07 -0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withBundleAnalyzer({
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    return config;
+  },
+  eslint: {
+    dirs: ['pages']
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  }
+});
