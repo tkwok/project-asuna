71704edc2833a5897bd96dc911393b0561ce2179
Tony Kwokm <holypaladin07@gmail.com>
2021-09-01 01:00:00 -0700

    minor updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true'
+  enabled: process.env.ANALYZE === 'true'
 });
 
 module.exports = withBundleAnalyzer({
