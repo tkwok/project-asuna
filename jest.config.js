89cf44113de02611597189afaeb54369f5032103
Tony Kwokm <holypaladin07@gmail.com>
2021-09-13 23:58:57 -0700

    minor jest change

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,8 @@ module.exports = {
     // Handle CSS imports (with CSS modules)
     // https://jestjs.io/docs/webpack#mocking-css-modules
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-
     // Handle CSS imports (without CSS modules)
     '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-
     // Handle image imports
     // https://jestjs.io/docs/webpack#handling-static-assets
     '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': `<rootDir>/__mocks__/fileMock.js`
@@ -27,5 +25,5 @@ module.exports = {
   transformIgnorePatterns: [
     '/node_modules/',
     '^.+\\.module\\.(css|sass|scss)$'
-  ],
+  ]
 }
