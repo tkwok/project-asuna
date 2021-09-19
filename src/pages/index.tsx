ab08444a9d9809a93c5ab16f89f64f88c4c0e050
Tony Kwokm <holypaladin07@gmail.com>
2021-09-19 00:02:04 -0700

    refactor and move pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 // components/ExampleDialog.js
 import { useState } from 'react';
 import { Navbar, Nav, NavDropdown, Container, Row, Col } from 'react-bootstrap';
-import Header from './Header';
+import Header from './common/Header';
 
 export default function indexPage(props) {
   return (
